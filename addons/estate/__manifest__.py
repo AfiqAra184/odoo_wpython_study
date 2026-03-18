d9fec4f9cee86af0abc23eca3833ebf11c92dc5f
Afiq Ara <afiqara184@gmail.com>
2026-03-18 13:54:10 +0800

    First upload: Odoo with Docker and GitHub Actions

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'A module to manage real estate listings',
+    'description': 'Training module for building a custom website backend',
+    'category': 'Sales',
+    'depends': ['base','crm'],
+    'data': [],
+    'application': True,
+}
